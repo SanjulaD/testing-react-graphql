4f574cba932d6b90d51ce9391f6df49f88c122e1
SanjulaD <desanjula99@gmail.com>
2021-09-05 12:39:04 +0530

    feat: users actions

--- fullstack_graphql/schema.py
+++ fullstack_graphql/schema.py
@@ -1,12 +1,11 @@
 import graphene
 import tracks.schema
+import users.schema
 
 class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
-
-class Mutation(tracks.schema.Mutations, graphene.ObjectType):
+class Mutation(users.schema.Mutations,tracks.schema.Mutations, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
