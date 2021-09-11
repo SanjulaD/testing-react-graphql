d011fecf5b144960d40e3a62ce7dda7a292269d6
SanjulaD <desanjula99@gmail.com>
2021-09-11 12:38:35 +0530

    feat: react apollo client and requiremtn txt files

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,6 +1,35 @@
 import React from "react";
 import withRoot from "./withRoot";
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-const Root = () => <div>Root</div>;
+const Root = () => (
+    <Query query={GET_TRACK_QUERY}>
+        {({ loading, data, error }) => {
+            if (loading) return <div>loading</div>
+            if (error) return <div>Error</div>
+
+            return (
+                <div>
+                    {console.log((data))}
+                    {
+                        data.tracks.map((item) => console.log(item))
+                    }
+                </div>
+            )
+        }}
+    </Query>
+);
+
+const GET_TRACK_QUERY = gql`
+{
+    tracks {
+        id
+        title
+        description
+        url
+    }
+}
+`
 
 export default withRoot(Root);
